661fd73ca0af1d99daf9d33b5d860a06da525b9e
Cesare83 <cesare.marchesi@yahoo.it>
2019-04-10 15:09:10 +0200

    2.5 added CRUD functions to Rest API

--- index.js
+++ index.js
@@ -1,29 +1,52 @@
 //------------------------------------------------------------------------------
 //-------------------------------IMPLEMENT PACKAGES-----------------------------
 //------------------------------------------------------------------------------
-//require packages
+
+//requiring packages
 const express = require('express'),
   morgan = require('morgan'),
   bodyParser = require('body-parser'),
   uuid = require('uuid');
-//running express enviroment
+
 const app = express();
+
+//route requests for static files to public folder
+app.use(express.static('public'));
+//invoke morgan => requests logged used Morgan´s common format!! (::1 - - [30/Nov/2018:05:43:09 +0000] 'GET /secreturl HTTP/1.1' 200 51)
+app.use(morgan('common'));
+//invoke bodyParser for POST requests
+app.use(bodyParser.json());
+
+//Error handling middleware func
+app.use((err, req, res, next) => {
+  console.error(err.stack);
+  res.status(500).send('Oops ..something broke!');
+});
 
 //------------------------------------------------------------------------------
 //----------------------IN-MEMORY OBJECTS FOR TESTING:--------------------------
 //------------------------------------------------------------------------------
 
+//Users-Objects
+let users = [
+  {
+    id: '1',
+    username : 'Cesare83',
+    password: 'password',
+    email: 'cesare.marchesi@yahoo.it',
+    birthday: ['23','10','1983']
+  }
+]
+
 //Directors-Objects
 let directors = [
   {
-    id: '1',
     name : 'uneven director',
     bio: 'uneven director biography',
     birth: 'uneven director´s birthday',
     death: 'still alive',
   },
   {
-    id: '2',
     name : 'even director',
     bio: 'even director biography',
     birth: 'even director´s birthday',
@@ -32,9 +55,8 @@ let directors = [
 ]
 
 //Movie-Objects
-let topMovies = [
+let favouriteMovies = [
   {
-    id: '1',
     title : 'first movie',
     description: 'first movie description',
     genre: 'first movie genre',
@@ -42,7 +64,6 @@ let topMovies = [
     image: 'first movie image'
   },
   {
-    id: '2',
     title : 'second movie',
     description: 'second movie description',
     genre: 'second movie genre',
@@ -50,7 +71,6 @@ let topMovies = [
     image: 'second movie image'
   },
   {
-    id: '3',
     title : 'third movie',
     description: 'third movie description',
     genre: 'third movie genre',
@@ -58,7 +78,6 @@ let topMovies = [
     image: 'third movie image'
   },
   {
-    id: '4',
     title : 'fourth movie',
     description: 'fourth movie description',
     genre: 'fourth movie genre',
@@ -66,7 +85,6 @@ let topMovies = [
     image: 'fourth movie image'
   },
   {
-    id: '5',
     title : 'fifth movie',
     description: 'fifth movie description',
     genre: 'fifth movie genre',
@@ -74,7 +92,6 @@ let topMovies = [
     image: 'fifth movie image'
   },
   {
-    id: '6',
     title : 'sixth movie',
     description: 'sixth movie description',
     genre: 'sixth movie genre',
@@ -82,7 +99,6 @@ let topMovies = [
     image: 'sixth movie image'
   },
   {
-    id: '7',
     title : 'seventh movie',
     description: 'seventh movie description',
     genre: 'seventh movie genre',
@@ -90,7 +106,6 @@ let topMovies = [
     image: 'seventh movie image'
   },
   {
-    id: '8',
     title : 'eighth movie',
     description: 'eighth movie description',
     genre: 'eighth movie genre',
@@ -98,7 +113,6 @@ let topMovies = [
     image: 'eighth movie image'
   },
   {
-    id: '9',
     title : 'nineth movie',
     description: 'nineth movie description',
     genre: 'nineth movie genre',
@@ -106,7 +120,6 @@ let topMovies = [
     image: 'nineth movie image'
   },
   {
-    id: '10',
     title : 'tenth movie',
     description: 'tenth movie description',
     genre: 'tenth movie genre',
@@ -119,85 +132,99 @@ let topMovies = [
 //-----------------------------FUNCTIONS HERE-----------------------------------
 //------------------------------------------------------------------------------
 
-//route requests for static files to public folder
-app.use(express.static('public'));
-//invoke morgan => requests logged used Morgan´s common format!! (::1 - - [30/Nov/2018:05:43:09 +0000] 'GET /secreturl HTTP/1.1' 200 51)
-app.use(morgan('common'));
-//Error handling middleware func
-app.use(function (err, req, res, next) {
-  console.error(err.stack);
-  res.status(500).send('Oops ..something broke!');
-});
-
-=======
-//require packages
-const express = require('express'),
-  morgan = require('morgan');
-//running express enviroment
-const app = express();
-
-//route requests for static files to public folder
-app.use(express.static('public'));
-//invoke morgan => requests logged used Morgan´s common format!! (::1 - - [30/Nov/2018:05:43:09 +0000] "GET /secreturl HTTP/1.1" 200 51)
-app.use(morgan('common'));
-//Error handling middleware func
-app.use(function (err, req, res, next) {
-  console.error(err.stack);
-  res.status(500).send('Oops ..something broke!');
-});
-
-//Test-Movie-Objects
-let topMovies = [
-  {
-  title : "first movie",
-  director: "first movie director"
-  },
-  {
-  title : "second movie",
-  director: "second movie director"
-  },
-  {
-  title : "third movie",
-  director: "third movie director"
-  },
-  {
-  title : "fourth movie",
-  director: "fourth movie director"
-  },
-  {
-  title : "fifth movie",
-  director: "fifth movie director"
-  },
-  {
-  title : "sixth movie",
-  director: "sixth movie director"
-  },
-  {
-  title : "seventh movie",
-  director: "seventh movie director"
-  },
-  {
-  title : "eighth movie",
-  director: "eight movie director"
-  },
-  {
-  title : "nineth movie",
-  director: "nineth movie director"
-  },
-  {
-  title : "tenth movie",
-  director: "tenth movie director"
-  }
-]
-
->>>>>>> 0d1d6bea8d43e011bd5c624bed5eb2f86f85d664
-//GET requests
-app.get('/', function(req, res) {
-  res.send('Welcome to myFlix!')
-});
-
-app.get('/movies', function(req, res) {
+//------------------------------GETTER FUNCTIONS--------------------------------
+//JSON-Obj with whole movies list
+app.get('/movies', (req, res) => {
   res.json(topMovies)
+});
+
+//JSON-Obj with movie details of selected movie
+app.get("/movies/:title", (req, res) => {
+  res.json(topMovies.find( (movie) => {
+    return movie.title === req.params.title
+  }));
+});
+
+//JSON-Obj with director details of selected director
+app.get("/directors/:name", (req, res) => {
+  res.json(directors.find( (director) => {
+    return director.name === req.params.name
+  }));
+});
+
+//-----------------------------POST FUNCTIONS-----------------------------------
+//create a new user
+app.post('/users', (req, res) => {
+  let newUser = req.body;
+
+  if (!newUser.username) {
+    const message = 'Missing username in request body';
+      res.status(400).send(message);
+  } else {
+    newUser.id = uuid.v4();
+    users.push(newUser);
+    res.status(201).send(newUser);
+  }
+});
+
+//create a new favouriteMovie
+app.post('/movies', (req, res) => {
+  let newFavouriteMovies = req.body;
+
+  if (!newFavouriteMovies.title) {
+    const message = 'Missing title in request body';
+      res.status(400).send(message);
+  } else {
+    favouriteMovies.push(newFavouriteMovie);
+    res.status(201).send(newFavouriteMovie);
+  }
+});
+
+//---------------------------------PUT FUNCTIONS--------------------------------
+//User´s details update by username
+app.put('/users/:username', (req, res) => {
+  let user = users.find((user) => {
+    return user.username === req.params.username
+  });
+
+  if (user) {
+    user.username = req.params.username;
+    user.password = req.params.password;
+    user.email = req.params.email;
+    user.birthday = req.params.birthday;
+    res.status(201).send('Personal data has been changed');
+  } else {
+    res.status(404).send('No user found with the name ' + req.params.username)
+  }
+});
+
+//-----------------------------DELETE FUNCTIONS---------------------------------
+//Delete user by ID
+app.delete("/users/:id", (req, res) => {
+  let user = users.find((user) => {
+     return user.id === req.params.id });
+
+  if (user) {
+    users.filter((obj) => {
+      return obj.id !== req.params.id });
+    res.status(201).send("User " + req.params.id + " was deleted.")
+  } else {
+    res.status(404).send('No user found with the name ' + req.params.username)
+  }
+});
+
+//Delete favourite movie by title
+app.delete('/movies/:title', (req, res) => {
+  let favouriteMovie = favouriteMovies.find((obj) =>
+    { return obj.title === req.params.title });
+
+  if (favouriteMovies) {
+    favouriteMovies.filter( (obj) => {
+      return obj.title !== req.params.title});
+      res.status(201).send(req.params.title + ' has been deleted.')
+  } else {
+    res.status(404).send('No movie found with the name ' + req.params.title)
+  }
 });
 
 //request listener
