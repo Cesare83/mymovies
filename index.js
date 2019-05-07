617e7de1127ac445b2cd6f656efd45ddc09f451a
Cesare83 <cesare.marchesi@yahoo.it>
2019-05-07 22:58:40 +0200

    2.10 connection

--- index.js
+++ index.js
@@ -39,7 +39,7 @@ app.use(morgan('common'));
 //route Mongoose to Database myMoviesDB LOCAL
 //mongoose.connect('mongodb://localhost:27017/myMoviesDB', {useNewUrlParser: true});
 
-route Mongoose to Database myMoviesDB REMOTE
+//route Mongoose to Database myMoviesDB REMOTE
 mongoose.connect('mongodb+srv://Cesare83:cesare#23@mymoviesb-frbwv.mongodb.net/mymoviesb?retryWrites=true', {useNewUrlParser: true});
 
 //Error handling middleware func
