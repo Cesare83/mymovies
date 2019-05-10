f802793321fd7b7c3495d5ef30166cfa5413c9e9
Cesare83 <cesare.marchesi@yahoo.it>
2019-05-10 09:18:42 +0200

    2.10 corrected typo

--- index.js
+++ index.js
@@ -40,7 +40,7 @@ app.use(morgan('common'));
 //mongoose.connect('mongodb://localhost:27017/myMoviesDB', {useNewUrlParser: true});
 
 //route Mongoose to Database myMoviesDB REMOTE
-const mongobUri = 'mongodb+srv://Cesare83:cesare#23@mymoviesb-frbwv.mongodb.net/mymoviesb?retryWrites=true';
+const mongodbUri = 'mongodb+srv://Cesare83:cesare#23@mymoviesb-frbwv.mongodb.net/mymoviesb?retryWrites=true';
 mongoose.connect(mongodbUri, {useNewUrlParser: true});
 
 //Error handling middleware func
