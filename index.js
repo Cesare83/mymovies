ad4bd5e5d0c0f8b84f1579a6fab495addc3f814b
Cesare83 <cesare.marchesi@yahoo.it>
2019-05-12 22:54:04 +0200

    2.10 connection

--- index.js
+++ index.js
@@ -41,7 +41,7 @@ app.use(morgan('common'));
 //mongoose.connect('mongodb://localhost:27017/myMoviesDB', {useNewUrlParser: true});
 
 //route Mongoose to Database myMoviesDB REMOTE
-mongoose.connect(process.env.mongodbUri), {useNewUrlParser: true});
+mongoose.connect((process.env.mongodbUri), {useNewUrlParser: true});
 
 //Error handling middleware func
 app.use((err, req, res, next) => {
