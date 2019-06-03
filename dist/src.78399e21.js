67743e7cf58979ed7a75533daada2fe66e1b33b6
Cesare83 <cesare.marchesi@yahoo.it>
2019-06-03 11:18:07 +0200

    3.3 updated back button prop

--- dist/src.78399e21.js
+++ dist/src.78399e21.js
@@ -27571,7 +27571,9 @@ function (_React$Component) {
   _createClass(MovieView, [{
     key: "render",
     value: function render() {
-      var movie = this.props.movie;
+      var _this$props = this.props,
+          movie = _this$props.movie,
+          _onClick = _this$props.onClick;
       if (!movie) return null;
       return _react.default.createElement("div", {
         className: "movie-view"
@@ -27603,19 +27605,9 @@ function (_React$Component) {
       }, "Director"), _react.default.createElement("div", {
         className: "value"
       }, movie.Director.Name)), _react.default.createElement("button", {
-        onClick: function (_onClick) {
-          function onClick() {
-            return _onClick.apply(this, arguments);
-          }
-
-          onClick.toString = function () {
-            return _onClick.toString();
-          };
-
-          return onClick;
-        }(function () {
-          return onClick();
-        })
+        onClick: function onClick() {
+          return _onClick();
+        }
       }, "Back to Movies"));
     }
   }]);
@@ -27898,7 +27890,7 @@ var parent = module.bundle.parent;
 if ((!parent || !parent.isParcelRequire) && typeof WebSocket !== 'undefined') {
   var hostname = "" || location.hostname;
   var protocol = location.protocol === 'https:' ? 'wss' : 'ws';
-  var ws = new WebSocket(protocol + '://' + hostname + ':' + "36125" + '/');
+  var ws = new WebSocket(protocol + '://' + hostname + ':' + "32951" + '/');
 
   ws.onmessage = function (event) {
     checkedAssets = {};
